41f3ea622fba942237209a51d35b8f24a9d5514b
Michael Connelly <michaelconne@gmail.com>
2021-11-06 17:53:11 -0500

    initial check for .net cdk action runner

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM mcr.microsoft.com/dotnet/sdk:5.0-alpine AS base
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
+RUN apk --update --no-cache add nodejs npm
+
 COPY entrypoint.sh /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,79 @@
 #!/bin/sh -l
+set -u
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+function parseInputs(){
+	# Required inputs
+	if [ "${INPUT_CDK_SUBCOMMAND}" == "" ]; then
+		echo "Input cdk_subcommand cannot be empty"
+		exit 1
+	fi
+}
+
+function installAwsCdk(){
+	echo "Install aws-cdk ${INPUT_CDK_VERSION}"
+	if [ "${INPUT_CDK_VERSION}" == "latest" ]; then
+		if [ "${INPUT_DEBUG_LOG}" == "true" ]; then
+			npm install -g aws-cdk
+		else
+			npm install -g aws-cdk >/dev/null 2>&1
+		fi
+
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-cdk ${INPUT_CDK_VERSION}"
+		else
+			echo "Successful install aws-cdk ${INPUT_CDK_VERSION}"
+		fi
+	else
+		if [ "${INPUT_DEBUG_LOG}" == "true" ]; then
+			npm install -g aws-cdk@${INPUT_CDK_VERSION}
+		else
+			npm install -g aws-cdk@${INPUT_CDK_VERSION} >/dev/null 2>&1
+		fi
+
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-cdk ${INPUT_CDK_VERSION}"
+		else
+			echo "Successful install aws-cdk ${INPUT_CDK_VERSION}"
+		fi
+	fi
+}
+
+function runCdk(){
+	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
+	set -o pipefail
+	cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1 | tee output.log
+	exitCode=${?}
+	set +o pipefail
+	echo ::set-output name=status_code::${exitCode}
+	output=$(cat output.log)
+
+	commentStatus="Failed"
+	if [ "${exitCode}" == "0" ]; then
+		commentStatus="Success"
+	elif [ "${exitCode}" != "0" ]; then
+		echo "CDK subcommand ${INPUT_CDK_SUBCOMMAND} for stack ${INPUT_CDK_STACK} has failed. See above console output for more details."
+		exit 1
+	fi
+
+	if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${INPUT_ACTIONS_COMMENT}" == "true" ]; then
+		commentWrapper="#### \`cdk ${INPUT_CDK_SUBCOMMAND}\` ${commentStatus}
+<details><summary>Show Output</summary>
+\`\`\`
+${output}
+\`\`\`
+</details>
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${INPUT_WORKING_DIR}\`*"
+
+		payload=$(echo "${commentWrapper}" | jq -R --slurp '{body: .}')
+		commentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+
+		echo "${payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${commentsURL}" > /dev/null
+	fi
+}
+
+function main(){
+	parseInputs
+	cd ${GITHUB_WORKSPACE}/${INPUT_WORKING_DIR}
+	installAwsCdk
+	runCdk ${INPUT_CDK_ARGS}
+}
